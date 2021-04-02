59a51e49c511fdfbf27193837281e56e8e9eb490
Carlosgdc <cdiazca@unal.edu.co>
2021-04-01 22:00:30 -0500

    Initial Commit

---
+++ Clase26-02-2021/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Clase26-02-2021
+//
+//  Created by Countyfoot on 4/1/21.
+//
+
+#include <iostream>
+
+int main(int arg, char **argv)
+{
+    std::cout << "Hola mundo!\n";
+    return 0;
+}
